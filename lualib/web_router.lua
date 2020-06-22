414de030413b65acbcb67ab8d924508d035ab048
涵曦 <hanxi.info@gmail.com>
2020-06-22 06:33:50 +0000

    add web router demo

--- lualib/web_router.lua
+++ lualib/web_router.lua
@@ -1,3 +1,5 @@
+local staticfile = require "staticfile"
+
 return function (r)
     r:get('/hw', function(params)
         return 'someone said hello'
@@ -5,7 +7,12 @@ return function (r)
     r:get('/', function(params)
         return 'index.html'
     end)
-    r:get('/static/:filename', function(params)
+    r:get('/static/:fname.:suffix', function(params)
+        print("static")
+        for k,v in pairs(params) do
+            print(type(k),#k,k,v)
+        end
+        local filename = string.format("%s.%s", params.fname, params.suffix)
         local content = staticfile[filename]
         if content then
             return content
