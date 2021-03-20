ad35df9a643051b63e934680a99361ad5afbdd2c
涵曦 <hanxi.info@gmail.com>
2021-03-21 07:59:00 +0800

    bugfix webroute for match static files

--- lualib/web_router.lua
+++ lualib/web_router.lua
@@ -7,12 +7,11 @@ return function (r)
     r:get('/', function(params)
         return 'index.html'
     end)
-    r:get('/static/:fname.:suffix', function(params)
+    r:get('/static/:filename', function(params)
         print("static")
         for k,v in pairs(params) do
             print(type(k),#k,k,v)
         end
-        local filename = string.format("%s.%s", params.fname, params.suffix)
         local content = staticfile[filename]
         if content then
             return content
