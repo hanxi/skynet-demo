ff999fb0977bbdabcac6522c1893f149b1f670c5
涵曦 <im.hanxi@gmail.com>
2022-01-24 11:34:43 +0800

    clean code

--- lualib/debug_console_inject.lua
+++ lualib/debug_console_inject.lua
@@ -1,41 +1,41 @@
 local source = [[
 local function getupvaluetable(u, func, unique)
     unique = unique or {}
-	local i = 1
-	while true do
-		local name, value = debug.getupvalue(func, i)
-		if name == nil then
-			return
-		end
-		local t = type(value)
-		if t == "table" then
-			u[name] = value
-		elseif t == "function" then
-			if not unique[value] then
-				unique[value] = true
-				getupvaluetable(u, value, unique)
-			end
-		end
-		i=i+1
-	end
+    local i = 1
+    while true do
+        local name, value = debug.getupvalue(func, i)
+        if name == nil then
+            return
+        end
+        local t = type(value)
+        if t == "table" then
+            u[name] = value
+        elseif t == "function" then
+            if not unique[value] then
+                unique[value] = true
+                getupvaluetable(u, value, unique)
+            end
+        end
+        i=i+1
+    end
 end
 
 local skynet = require "skynet"
 local TIMEOUT = 300 -- 3 sec
 local function timeout(ti)
-	if ti then
-		ti = tonumber(ti)
-		if ti <= 0 then
-			ti = nil
-		end
-	else
-		ti = TIMEOUT
-	end
-	return ti
+    if ti then
+        ti = tonumber(ti)
+        if ti <= 0 then
+            ti = nil
+        end
+    else
+        ti = TIMEOUT
+    end
+    return ti
 end
 local function stat(ti)
-	local statlist = skynet.call(".launcher", "lua", "STAT", timeout(ti))
-	local memlist = skynet.call(".launcher", "lua", "MEM", timeout(ti))
+    local statlist = skynet.call(".launcher", "lua", "STAT", timeout(ti))
+    local memlist = skynet.call(".launcher", "lua", "MEM", timeout(ti))
     local memkv = {}
     for k,v in pairs(memlist) do
         memkv[k] = v
